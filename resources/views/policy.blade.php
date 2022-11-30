eb31442cde3122e55ae45ab8abe948076822d321
Benjamin Takats <mail@benjamintakats.de>
2022-11-30 17:19:02 +0100

    login and register links added

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="pt-4 bg-gray-100">
+    <div class="pt-4 bg-21gray">
         <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
             <div>
                 <x-jet-authentication-card-logo />
