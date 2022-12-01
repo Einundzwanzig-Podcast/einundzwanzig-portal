9dac3cde4604b8c2f6071ae1f63278b802718f65
Benjamin Takats <mail@benjamintakats.de>
2022-12-01 19:49:24 +0100

    ln auth added

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,32 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/lnurl-auth-callback', function (\Illuminate\Http\Request $request) {
+    if (lnurl\auth($request->k1, $request->signature, $request->wallet_public_key)) {
+        // find User by $wallet_public_key
+        $user = User::where('public_key', $request->key)
+                    ->first();
+        if (!$user) {
+            // create User
+            $user = User::create([
+                'public_key'  => $request->wallet_public_key,
+                'is_lecturer' => true,
+            ]);
+        }
+        // check if $k1 is in the database, if not, add it
+        $loginKey = LoginKey::where('k1', $request->k1)
+                            ->first();
+        if (!$loginKey) {
+            LoginKey::create([
+                'k1'      => $request->k1,
+                'user_id' => $user->id,
+            ]);
+        }
+
+        return response()->json(['status' => 'OK']);
+    }
+
+    return response()->json(['status' => 'ERROR', 'reason' => 'Signature was NOT VERIFIED']);
+})
+     ->name('auth.ln.callback');
--- routes/web.php
+++ routes/web.php
@@ -24,35 +24,6 @@ Route::get('/', function () {
 Route::get('/auth/ln', \App\Http\Livewire\Auth\LNUrlAuth::class)
      ->name('auth.ln');
 
-Route::get('/lnurl-auth-callback', function (\Illuminate\Http\Request $request) {
-    if (lnurl\auth($request->k1, $request->signature, $request->wallet_public_key)) {
-        // find User by $wallet_public_key
-        $user = User::where('public_key', $request->key)
-                    ->first();
-        if (!$user) {
-            // create User
-            $user = User::create([
-                'public_key'  => $request->wallet_public_key,
-                'is_lecturer' => true,
-            ]);
-        }
-        // check if $k1 is in the database, if not, add it
-        $loginKey = LoginKey::where('k1', $request->k1)
-                            ->first();
-        if (!$loginKey) {
-            LoginKey::create([
-                'k1'      => $request->k1,
-                'user_id' => $user->id,
-            ]);
-        }
-
-        return response()->json(['status' => 'OK']);
-    }
-
-    return response()->json(['status' => 'ERROR', 'reason' => 'Signature was NOT VERIFIED']);
-})
-     ->name('auth.ln.callback');
-
 Route::get('/{country:code}/suche/stadt', \App\Http\Livewire\Frontend\SearchCity::class)
      ->name('search.city');
 
