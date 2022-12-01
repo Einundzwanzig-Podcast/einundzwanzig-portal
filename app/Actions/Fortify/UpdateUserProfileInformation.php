b1e0cdba866949e03153bf484d35e41251184280
Benjamin Takats <mail@benjamintakats.de>
2022-12-01 20:59:46 +0100

    profile update changed

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -21,7 +21,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     {
         Validator::make($input, [
             'name'  => ['required', 'string', 'max:255'],
-            'email' => ['email', 'max:255', Rule::unique('users')
+            'email' => ['nullable', 'email', 'max:255', Rule::unique('users')
                                                 ->ignore($user->id)
             ],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
