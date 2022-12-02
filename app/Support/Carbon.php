2abfa81c88d7649249f57bf420fa1b4a9ce54723
Benjamin Takats <mail@benjamintakats.de>
2022-12-02 16:58:55 +0100

    td changed

--- app/Support/Carbon.php
+++ app/Support/Carbon.php
@@ -21,9 +21,9 @@ class Carbon extends CarbonImmutable
     public function asDateTime(): string
     {
         $dt = $this->timezone(config('app.timezone'))->locale('de');
-        return sprintf("%s. %s %s %s (%s)",
+        return sprintf("%s.%s.%s %s (%s)",
             str($dt->day)->padLeft(2, '0'),
-            $dt->monthName,
+            str($dt->month)->padLeft(2, '0'),
             $dt->year,
             $dt->format('H:i'),
             $dt->timezone
