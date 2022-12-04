0581d00c3e24afc5dd80d20ccaf5672f22ad7d0f
Benjamin Takats <mail@benjamintakats.de>
2022-12-04 14:03:22 +0100

    tabs changed

--- resources/views/livewire/frontend/search-tabs.blade.php
+++ resources/views/livewire/frontend/search-tabs.blade.php
@@ -2,18 +2,6 @@
     <div class="border-b border-gray-200 pb-5 sm:pb-0 my-6">
         <h3 class="text-lg font-medium leading-6 text-gray-200">Suche</h3>
         <div class="mt-3 sm:mt-4">
-            <!-- Dropdown menu on small screens -->
-            <div class="sm:hidden">
-                <label for="current-tab" class="sr-only">Select a tab</label>
-                <select id="current-tab" name="current-tab"
-                        class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                    <option {{ route('search.city', ['country' => $country]) ? 'selected' : '' }}>Städte</option>
-                    <option {{ route('search.lecturer', ['country' => $country]) ? 'selected' : '' }}>Dozenten</option>
-                    <option {{ route('search.venue', ['country' => $country]) ? 'selected' : '' }}>Veranstalungs-Orte
-                    </option>
-                    <option {{ route('search.course', ['country' => $country]) ? 'selected' : '' }}>Kurse</option>
-                </select>
-            </div>
             <!-- Tabs at small breakpoint and up -->
             <div class="hidden sm:block">
                 <nav class="-mb-px flex space-x-8">
