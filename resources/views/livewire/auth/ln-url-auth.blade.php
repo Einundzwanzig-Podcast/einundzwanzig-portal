91cecf1ffe9ca757fd39d401e1b45f79d2dacf64
Benjamin Takats <mail@benjamintakats.de>
2022-12-05 21:02:15 +0100

    ln auth as default

--- resources/views/livewire/auth/ln-url-auth.blade.php
+++ resources/views/livewire/auth/ln-url-auth.blade.php
@@ -9,17 +9,33 @@
                 {!! $this->qrCode !!}
             </div>
             <div class="my-2 flex justify-center font-mono break-all">
-                <span wire:key="lnurl">lightning:{{ $this->lnurl }}</span>
+                <input class="w-full" readonly wire:key="lnurl" onClick="this.select();"
+                       value="lightning:{{ $this->lnurl }}"/>
+            </div>
+            <div class="flex justify-end w-full">
+                <div
+                    x-data="{
+                  textToCopy: 'lightning:{{ $this->lnurl }}',
+                }"
+                    @click.prevent="window.navigator.clipboard.writeText(textToCopy);window.$wireui.notify({title:'URL kopiert!',icon:'success'});"
+                >
+                    <x-button
+                        xs
+                    >
+                        <i class="fa fa-thin fa-clipboard"></i>
+                        Kopiere
+                    </x-button>
+                </div>
             </div>
         </div>
 
         <div class="flex items-center justify-end mt-4">
 
-            <x-button icon="login" secondary class="ml-4" wire:click="switchToEmail">
+            <x-button icon="login" secondary class="ml-4" wire:click="switchToEmailLogin">
                 {{ __('Switch to E-Mail login') }}
             </x-button>
 
-            <x-button icon="at-symbol" primary class="ml-4" wire:click="switchToEmail">
+            <x-button icon="at-symbol" primary class="ml-4" wire:click="switchToEmailSignup">
                 {{ __('Switch to E-Mail signup') }}
             </x-button>
 
