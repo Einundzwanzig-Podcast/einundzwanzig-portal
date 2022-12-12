c8e9a47a4e0dffb9e18d64add9ee9b8f3280274e
Benjamin Takats <mail@benjamintakats.de>
2022-12-12 21:01:50 +0100

    email disabled

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -62,7 +62,7 @@
         </div>
 
         <!-- Email -->
-        <div class="col-span-6 sm:col-span-4">
+        {{--<div class="col-span-6 sm:col-span-4">
             <x-jet-label for="email" value="{{ __('Email') }}"/>
             <x-jet-input id="email" type="email" class="mt-1 block w-full" wire:model.defer="state.email"/>
             <x-jet-input-error for="email" class="mt-2"/>
@@ -83,7 +83,7 @@
                     </p>
                 @endif
             @endif
-        </div>
+        </div>--}}
 
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="timezone" value="{{ __('Timezone') }}"/>
