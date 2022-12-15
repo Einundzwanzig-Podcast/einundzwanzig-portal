10db4da13ec57b352add6cec7563aaefaa6c09a4
Benjamin Takats <mail@benjamintakats.de>
2022-12-15 11:11:06 +0100

    delete defaults

--- database/migrations/2022_12_14_123437_create_created_by_fields_on_all_tables.php
+++ database/migrations/2022_12_14_123437_create_created_by_fields_on_all_tables.php
@@ -13,91 +13,78 @@ return new class extends Migration {
     {
         Schema::table('cities', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('venues', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('lecturers', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('meetups', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('meetup_events', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('bitcoin_events', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('courses', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('course_events', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('libraries', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('library_items', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('podcasts', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('episodes', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
         });
         Schema::table('book_cases', function (Blueprint $table) {
             $table->foreignId('created_by')
-                  ->default(1)
                   ->constrained('users')
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
