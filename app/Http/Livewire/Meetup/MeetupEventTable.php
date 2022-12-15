d40efbf521c2cdec907e8e23bb91220e26e3b60f
Benjamin Takats <mail@benjamintakats.de>
2022-12-15 18:47:14 +0100

    calendar added

--- app/Http/Livewire/Meetup/MeetupEventTable.php
+++ app/Http/Livewire/Meetup/MeetupEventTable.php
@@ -3,13 +3,30 @@
 namespace App\Http\Livewire\Meetup;
 
 use App\Models\Country;
+use App\Models\MeetupEvent;
 use Livewire\Component;
+use WireUi\Traits\Actions;
 
 class MeetupEventTable extends Component
 {
+    use Actions;
+
     public Country $country;
+
     public function render()
     {
-        return view('livewire.meetup.meetup-event-table');
+        return view('livewire.meetup.meetup-event-table', [
+            'events' => MeetupEvent::query()->get()->map(fn($event) => [
+                'startDate' => $event->start,
+                'endDate' => $event->start->endOfDay(),
+                'location' => $event->location,
+                'description' => $event->description,
+            ]),
+        ]);
+    }
+
+    public function popover($content)
+    {
+        $this->notification()->success($content);
     }
 }
