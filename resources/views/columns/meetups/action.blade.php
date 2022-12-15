7efcf8591cbf5a1119be74bf754d22310fef03ab
Benjamin Takats <mail@benjamintakats.de>
2022-12-15 15:11:53 +0100

    outlined

--- resources/views/columns/meetups/action.blade.php
+++ resources/views/columns/meetups/action.blade.php
@@ -1,1 +1,16 @@
-<x-button amber wire:click="meetupEventSearch({{ $row->id }})">Termine anzeigen ({{ $row->meetup_events_count }})</x-button>
+<div>
+    @if($row->meetup_events_count > 0)
+        <x-button
+            amber
+            wire:click="meetupEventSearch({{ $row->id }})">
+            Termine anzeigen ({{ $row->meetup_events_count }})
+        </x-button>
+    @endif
+    @if($row->meetup_events_count < 1)
+        <x-button
+            outlined
+            wire:click="meetupEventSearch({{ $row->id }})">
+            Termine anzeigen ({{ $row->meetup_events_count }})
+        </x-button>
+    @endif
+</div>
