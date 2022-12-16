3067b43e4a932d3cb02ec5de961afe8089eca704
Benjamin Takats <mail@benjamintakats.de>
2022-12-16 12:08:24 +0100

    flags added

--- app/Http/Livewire/Tables/BitcoinEventTable.php
+++ app/Http/Livewire/Tables/BitcoinEventTable.php
@@ -18,7 +18,10 @@ class BitcoinEventTable extends DataTableComponent
     {
         $this->setPrimaryKey('id')
              ->setDefaultSort('from', 'asc')
-             ->setAdditionalSelects(['bitcoin_events.id'])
+             ->setAdditionalSelects([
+                 'bitcoin_events.id',
+                 'bitcoin_events.venue_id'
+             ])
              ->setThAttributes(function (Column $column) {
                  return [
                      'class'   => 'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:bg-gray-800 dark:text-gray-400',
@@ -49,6 +52,11 @@ class BitcoinEventTable extends DataTableComponent
     public function columns(): array
     {
         return [
+            Column::make(__('Country'), 'venue.city.country.name')
+                  ->format(
+                      fn($value, $row, Column $column) => view('columns.bitcoin_events.country')->withRow($row)
+                  )
+                  ->sortable(),
             Column::make(__('Title'), 'title')
                   ->format(
                       fn($value, $row, Column $column) => view('columns.bitcoin_events.title')->withRow($row)
@@ -74,6 +82,9 @@ class BitcoinEventTable extends DataTableComponent
     public function builder(): Builder
     {
         return BitcoinEvent::query()
+                           ->with([
+                               'venue.city.country'
+                           ])
                            ->where(fn($query) => $query
                                ->whereHas('venue.city.country',
                                    fn($query) => $query->where('code', $this->country))
