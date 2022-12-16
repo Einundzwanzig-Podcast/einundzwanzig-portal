01092132bf113b64333b195839cf1a07ef4fec39
Benjamin Takats <mail@benjamintakats.de>
2022-12-16 11:59:46 +0100

    counts added

--- resources/views/columns/lectures/action.blade.php
+++ resources/views/columns/lectures/action.blade.php
@@ -1,10 +1,26 @@
 <div>
-    <x-button amber wire:click="lecturerSearch({{ $row->id }})">
-        <i class="fa fa-thin fa-calendar mr-2"></i>
-        Termine anzeigen
-    </x-button>
-    <x-button amber wire:click="lecturerSearch({{ $row->id }})">
-        <i class="fa fa-thin fa-book mr-2"></i>
-        Inhalte anzeigen
-    </x-button>
+    @if($row->courses_count > 0)
+        <x-button amber wire:click="lecturerSearch({{ $row->id }})">
+            <i class="fa fa-thin fa-calendar mr-2"></i>
+            Termine anzeigen ({{ $row->courses_count }})
+        </x-button>
+    @endif
+    @if($row->courses_count < 1)
+        <x-button outlined wire:click="lecturerSearch({{ $row->id }})">
+            <i class="fa fa-thin fa-calendar mr-2"></i>
+            Termine anzeigen ({{ $row->courses_count }})
+        </x-button>
+    @endif
+    @if($row->library_items_count > 0)
+        <x-button amber wire:click="lecturerSearch({{ $row->id }})">
+            <i class="fa fa-thin fa-book mr-2"></i>
+            Inhalte anzeigen ({{ $row->library_items_count }})
+        </x-button>
+    @endif
+    @if($row->library_items_count < 1)
+        <x-button outlined wire:click="lecturerSearch({{ $row->id }})">
+            <i class="fa fa-thin fa-book mr-2"></i>
+            Inhalte anzeigen ({{ $row->library_items_count }})
+        </x-button>
+    @endif
 </div>
