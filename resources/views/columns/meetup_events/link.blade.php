5a31b57df1de5970958841823d568857e243afab
Benjamin Takats <mail@benjamintakats.de>
2022-12-16 11:54:53 +0100

    link badges changed

--- resources/views/columns/meetup_events/link.blade.php
+++ resources/views/columns/meetup_events/link.blade.php
@@ -1,5 +1,8 @@
 <div>
     <a href="{{ $row->link }}" target="_blank">
-        <x-badge class="whitespace-nowrap">{{ $row->link }}</x-badge>
+        <x-badge class="whitespace-nowrap">
+            <i class="fa fa-thin fa-external-link mr-2"></i>
+            Öffnen
+        </x-badge>
     </a>
 </div>
