bc870c6915dc0458a6cc41e8dc85f145ea9e11b7
Benjamin Takats <mail@benjamintakats.de>
2022-12-16 12:25:29 +0100

    course_events_count

--- resources/views/columns/venues/action.blade.php
+++ resources/views/columns/venues/action.blade.php
@@ -1,1 +1,12 @@
-<x-button amber wire:click="venueSearch({{ $row->id }})">Termine anzeigen</x-button>
+<div>
+    @if($row->course_events_count > 0)
+        <x-button amber wire:click="venueSearch({{ $row->id }})">
+            Termine anzeigen ({{ $row->course_events_count }})
+        </x-button>
+    @endif
+    @if($row->course_events_count < 1)
+        <x-button outlined wire:click="venueSearch({{ $row->id }})">
+            Termine anzeigen ({{ $row->course_events_count }})
+        </x-button>
+    @endif
+</div>
