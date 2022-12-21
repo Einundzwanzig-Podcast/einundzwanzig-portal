69e5cc2ea70821501390d8a3c80c07b49b99502f
Benjamin Takats <mail@benjamintakats.de>
2022-12-21 18:35:26 +0100

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="apple-touch-icon" href="/img/apple_touch_icon.png"/>
-    <link rel="icon" href="/img/favicon.svg"/>
-    {!! seo() !!}
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    {!! seo($SEOData) !!}
     <!-- Fonts -->
     @googlefonts
     <!-- Scripts -->
