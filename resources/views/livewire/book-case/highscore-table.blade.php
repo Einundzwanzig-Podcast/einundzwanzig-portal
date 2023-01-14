d38a62d0c171dc8284f24240c820a1391dac7c62
Benjamin Takats <mail@benjamintakats.de>
2023-01-14 21:53:42 +0100

    object-cover

--- resources/views/livewire/book-case/highscore-table.blade.php
+++ resources/views/livewire/book-case/highscore-table.blade.php
@@ -21,7 +21,7 @@
                 @foreach($plebs as $pleb)
                     <li class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-amber-200 text-center shadow">
                         <div class="flex flex-1 flex-col p-8">
-                            <img class="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
+                            <img class="mx-auto h-32 w-32 object-cover flex-shrink-0 rounded-full"
                                  src="{{ $pleb->profile_photo_url }}" alt="{{ $pleb->name }}">
                             <h3 class="mt-6 text-sm font-medium text-gray-900 truncate">{{ $pleb->public_key }}</h3>
                             <dl class="mt-1 flex flex-grow flex-col justify-between">
