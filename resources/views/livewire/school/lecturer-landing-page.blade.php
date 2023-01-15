bceae0617678c7ed991368dedbc70ab6da8ff8ef
Benjamin Takats <mail@benjamintakats.de>
2023-01-15 14:54:40 +0100

    sort by coursesEvents

--- resources/views/livewire/school/lecturer-landing-page.blade.php
+++ resources/views/livewire/school/lecturer-landing-page.blade.php
@@ -3,7 +3,7 @@
     <section class="w-full mb-12 mt-8">
         <div class="max-w-screen-2xl mx-auto px-2 sm:px-10 space-y-4 flex flex-col sm:flex-row">
             <div class="flex flex-col sm:flex-row sm:space-x-6">
-                <div class="flex flex-col">
+                <div class="sm:w-2/3 flex flex-col">
                     <h1 class="mb-6 text-5xl font-extrabold leading-none tracking-normal text-gray-200 sm:text-6xl md:text-6xl lg:text-7xl md:tracking-tight">
                         <span
                             class="text-transparent bg-clip-text bg-gradient-to-r from-amber-400 via-amber-500 to-amber-200">{{ $lecturer->name }}</span><br
@@ -15,7 +15,7 @@
                     </div>
                 </div>
 
-                <div class="p-4">
+                <div class="sm:w-1/3 p-4">
                     <img class="max-h-64" src="{{ $lecturer->getFirstMediaUrl('avatar') }}" alt="Avatar">
                 </div>
             </div>
