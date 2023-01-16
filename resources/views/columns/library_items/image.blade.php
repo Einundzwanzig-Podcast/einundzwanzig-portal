057f1bdeb07cd45620d621323f85982a96cef9c8
Benjamin Takats <mail@benjamintakats.de>
2023-01-16 12:57:22 +0100

    rounded w-16 h-16 object-cover

--- resources/views/columns/library_items/image.blade.php
+++ resources/views/columns/library_items/image.blade.php
@@ -1,3 +1,3 @@
-<div class="flex items-center space-x-2">
-    <img class="w-12 h-12 object-cover" src="{{ $row->getFirstMediaUrl('main', 'thumb') }}" alt="{{ $row->name }}">
+<div class="w-12 h-12">
+    <img class="object-cover" src="{{ $row->getFirstMediaUrl('main', 'thumb') }}" alt="{{ $row->name }}">
 </div>
