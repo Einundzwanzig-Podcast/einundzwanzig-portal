f6d9b868d589550aa41c6eb37dc167390789f9c6
Benjamin Takats <mail@benjamintakats.de>
2023-01-17 20:17:51 +0100

    sort

--- app/Http/Livewire/Tables/MeetupTable.php
+++ app/Http/Livewire/Tables/MeetupTable.php
@@ -17,6 +17,7 @@ class MeetupTable extends DataTableComponent
     public function configure(): void
     {
         $this->setPrimaryKey('id')
+             ->setDefaultSort('users_count', 'desc')
              ->setThAttributes(function (Column $column) {
                  return [
                      'class'   => 'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:bg-gray-800 dark:text-gray-400',
