a38005859ce9ee732ace460951ba23806465ac3c
Benjamin Takats <mail@benjamintakats.de>
2023-01-17 21:53:44 +0100

    better layout

--- resources/views/livewire/auth/ln-url-auth.blade.php
+++ resources/views/livewire/auth/ln-url-auth.blade.php
@@ -52,6 +52,121 @@
         @endif
 
         <div class="pt-12">
+            {{ __('Kompatible Wallets:') }}
+        </div>
+        <div class="pt-2">
+            <td
+                class="py-1 px-3 text-base leading-6 break-words border border-solid border-collapse border-neutral-600 text-slate-400"
+                style="list-style: outside;"
+            >
+                <a
+                    href="https://github.com/getAlby/lightning-browser-extension"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >Alby</a
+                >,
+                <a
+                    href="https://github.com/alexbosworth/balanceofsatoshis"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >Balance of Satoshis</a
+                >,
+                <a href="https://blixtwallet.github.io" class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                   style="text-decoration: none; list-style: outside;">Blixt</a>,
+                <a
+                    href="https://breez.technology"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >Breez</a
+                >,
+                <a
+                    href="https://bluewallet.io"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >BlueWallet</a
+                >,
+                <a
+                    href="https://coinos.io"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >coinos</a
+                >,
+                <a
+                    href="https://geyser.fund"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >Geyser</a
+                >, <a href="https://lifpay.me"
+                      rel="nofollow"
+                      class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                      style="text-decoration: none; list-style: outside;">LifPay</a>,
+                <a
+                    href="https://lnbits.com"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >LNbits</a
+                >, <a href="https://lntxbot.com"
+                      rel="nofollow"
+                      class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                      style="text-decoration: none; list-style: outside;">@lntxbot</a>,
+                <a
+                    href="https://ln.tips"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >LightningTipBot</a
+                >, <a href="https://phoenix.acinq.co"
+                      rel="nofollow"
+                      class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                      style="text-decoration: none; list-style: outside;">Phoenix</a>,
+                <a
+                    href="https://seedauth.etleneum.com/"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >SeedAuth</a
+                >,
+                <a
+                    href="https://github.com/pseudozach/seedauthextension"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >SeedAuthExtension</a
+                >,
+                <a href="https://lightning-wallet.com"
+                   rel="nofollow"
+                   class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                   style="text-decoration: none; list-style: outside;">SimpleBitcoinWallet</a>,
+                <a href="https://sparrowwallet.com/"
+                   rel="nofollow"
+                   class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                   style="text-decoration: none; list-style: outside;">Sparrow Wallet</a>,
+                <a
+                    href="https://www.thunderhub.io"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >ThunderHub</a
+                >,
+                <a
+                    href="https://zaphq.io/"
+                    rel="nofollow"
+                    class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                    style="text-decoration: none; list-style: outside;"
+                >Zap Desktop</a
+                >, <a href="https://zeusln.app"
+                      rel="nofollow"
+                      class="leading-6 text-blue-400 bg-transparent cursor-pointer"
+                      style="text-decoration: none; list-style: outside;">Zeus</a>
+            </td>
+
+        </div>
+
+        <div class="pt-12">
             <p class="text-xs">{{ __('Zeus bug:') }} <a target="_blank"
                                                         href="https://github.com/ZeusLN/zeus/issues/1045">https://github.com/ZeusLN/zeus/issues/1045</a>
             </p>
