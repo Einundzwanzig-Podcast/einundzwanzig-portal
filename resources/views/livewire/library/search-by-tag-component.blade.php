bb1bf94a1b1f21b20eace778ceb4611b86979b5d
Benjamin Takats <mail@benjamintakats.de>
2023-01-21 18:32:28 +0100

    search

--- resources/views/livewire/library/search-by-tag-component.blade.php
+++ resources/views/livewire/library/search-by-tag-component.blade.php
@@ -1,9 +1,9 @@
 <div
-    class="flex overflow-auto relative flex-wrap gap-x-5 gap-y-1 justify-center p-0 mx-auto mt-2 mb-2 w-full font-normal text-white align-baseline border-0 border-solid md:mx-auto md:mb-0 md:max-w-screen-2xl"
+    class="flex overflow-auto relative flex-wrap gap-x-1 gap-y-1 justify-left p-0 mx-auto mt-2 mb-2 w-full font-normal text-white align-baseline border-0 border-solid md:mx-auto md:mb-0 md:max-w-screen-2xl"
 >
     @foreach($tags as $tag)
         <div
-            class="flex flex-wrap justify-center p-0 m-0 text-center align-baseline border-0 border-solid"
+            class="flex flex-wrap justify-left p-0 m-0 text-center align-baseline border-0 border-solid"
             style="font-size: 128%; background-position: 0px center; list-style: outside;"
         >
             <a
