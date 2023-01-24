a90a46679c3b229e1b57577c2799bf16c68dde3a
Benjamin Takats <mail@benjamintakats.de>
2023-01-24 18:56:42 +0100

    add podcasts

--- resources/views/livewire/news/intern-article-view.blade.php
+++ resources/views/livewire/news/intern-article-view.blade.php
@@ -87,7 +87,7 @@
                 </div>
                 <div class="mt-8 lg:mt-0">
                     <div class="mx-auto max-w-prose text-base lg:max-w-none">
-                        <p class="text-lg text-gray-200 prose md:prose-lg prose-invert">{!! $libraryItem->subtitle !!}</p>
+                        <div class="prose md:prose-lg prose-invert">{!! $libraryItem->subtitle !!}</div>
                     </div>
                     <div
                         class="prose md:prose-lg prose-invert mx-auto mt-5 text-gray-100 lg:col-start-1 lg:row-start-1 lg:max-w-none">
