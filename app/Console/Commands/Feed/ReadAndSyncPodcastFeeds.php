52204f8ce049456634c512a0a88b89b09423fe0e
Benjamin Takats <mail@benjamintakats.de>
2023-01-25 16:35:30 +0100

    new feed

--- app/Console/Commands/Feed/ReadAndSyncPodcastFeeds.php
+++ app/Console/Commands/Feed/ReadAndSyncPodcastFeeds.php
@@ -47,6 +47,15 @@ class ReadAndSyncPodcastFeeds extends Command
             1769616, // Einemillionsatoshi - der Bitcoin Podcast für Frauen
             5784083, // Der Bitcoin Effekt - Dein Business Podcast
             5932490, // Satoshis Coffeeshop
+            5872859, // Zeitsprung Bitcoin
+            5285606, // Die Bitcoin Lesestunde
+            5910857, // SOS - Shield Of Satoshi - Ein Bitcoin Podcast
+            5045045, // Bitcoin Bibliothek
+            4138995, // Orange Relationship
+            5326181, // Powering Bitcoin - Bitcoin & Energy
+            5783120, // Bitcoin´s Energie und Zeit
+            558916, // Bitcoin verstehen
+            5248065, // Bitcoin Sozial – Lesestunde
         ];
 
         foreach ($feedIds as $feedId) {
