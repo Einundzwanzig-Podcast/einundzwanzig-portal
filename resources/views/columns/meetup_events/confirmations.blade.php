0330f996820547de7372d05769774e4d688a5ca3
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-07 16:36:02 +0100

    column added

--- resources/views/columns/meetup_events/confirmations.blade.php
+++ resources/views/columns/meetup_events/confirmations.blade.php
@@ -1,8 +1,18 @@
 <div class="flex flex-col space-y-1">
     <div>
-        <x-badge class="whitespace-nowrap">{{ __('Confirmations') }}: {{ count($row->attendees ?? []) }}</x-badge>
+        @if(count($row->attendees ?? []) > 0)
+            <x-badge green class="whitespace-nowrap">{{ __('Confirmations') }}
+                : {{ count($row->attendees ?? []) }}</x-badge>
+        @else
+            <x-badge class="whitespace-nowrap">{{ __('Confirmations') }}: {{ count($row->attendees ?? []) }}</x-badge>
+        @endif
     </div>
     <div>
-        <x-badge class="whitespace-nowrap">{{ __('Perhaps') }}: {{ count($row->might_attendees ?? []) }}</x-badge>
+        @if(count($row->might_attendees ?? []) > 0)
+            <x-badge green class="whitespace-nowrap">{{ __('Perhaps') }}
+                : {{ count($row->might_attendees ?? []) }}</x-badge>
+        @else
+            <x-badge class="whitespace-nowrap">{{ __('Perhaps') }}: {{ count($row->might_attendees ?? []) }}</x-badge>
+        @endif
     </div>
 </div>
