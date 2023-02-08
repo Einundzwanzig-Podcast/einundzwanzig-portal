11fee00a988e154ad5658b5e3ae3eef1b645bcbb
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-08 10:51:12 +0100

    heatmap changed

--- resources/views/livewire/book-case/heatmap.blade.php
+++ resources/views/livewire/book-case/heatmap.blade.php
@@ -20,7 +20,7 @@
                         var baseLayer = L.tileLayer(
                             'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',{
                                 attribution: 'Map tiles by <a href=\'http://stamen.com\'>Stamen Design</a>, <a href=\'http://creativecommons.org/licenses/by/3.0\'>CC BY 3.0</a> &mdash; Map data &copy; <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a>',
-                                maxZoom: 18
+                                maxZoom: 16
                             }
                         );
 
