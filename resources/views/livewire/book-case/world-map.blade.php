85364f9bed61f4eaf025198e83b3f22221d763d6
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-08 21:18:29 +0100

    lang changed

--- resources/views/livewire/book-case/world-map.blade.php
+++ resources/views/livewire/book-case/world-map.blade.php
@@ -17,8 +17,8 @@
                     init() {
 
                         var baseLayer = L.tileLayer(
-                            'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',{
-                                attribution: 'Map tiles by <a href=\'http://stamen.com\'>Stamen Design</a>, <a href=\'http://creativecommons.org/licenses/by/3.0\'>CC BY 3.0</a> &mdash; Map data &copy; <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a>',
+                            'https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png',{
+                                attribution: '&copy; <a href=\'https://www.openstreetmap.org/copyright\'>OpenStreetMap</a> contributors',
                             }
                         );
 
