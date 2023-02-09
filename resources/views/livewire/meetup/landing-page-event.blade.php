74244602cc692e495baa19a1af79ef4a47d7ccf9
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-09 11:16:14 +0100

    markdown fix

--- resources/views/livewire/meetup/landing-page-event.blade.php
+++ resources/views/livewire/meetup/landing-page-event.blade.php
@@ -60,7 +60,7 @@
                                 <p class="font-bold text-xl">
                                     {{ __('Where') }}: {{ $meetupEvent->location }}
                                 </p>
-                                <div class="prose prose-white">
+                                <div class="prose prose-white leading-normal">
                                     <x-markdown>
                                         {!! $meetupEvent->description !!}
                                     </x-markdown>
