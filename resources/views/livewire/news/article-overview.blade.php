4b63d822eb205505cf8808936e17bee2a557d1f2
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-11 13:43:14 +0100

    news edit

--- resources/views/livewire/news/article-overview.blade.php
+++ resources/views/livewire/news/article-overview.blade.php
@@ -65,7 +65,7 @@
                                         @endif
                                     </div>
                                     <div class="flex space-x-1 text-sm text-gray-500 justify-end">
-                                        @if($libraryItem->created_by == auth()->id())
+                                        @if($libraryItem->created_by == auth()->id() || auth()->user()->hasRole('news-editor'))
                                             <x-button :href="route('news.form', ['libraryItem' => $libraryItem])">
                                                 <i class="fa fa-thin fa-edit"></i>
                                                 {{ __('Edit') }}
