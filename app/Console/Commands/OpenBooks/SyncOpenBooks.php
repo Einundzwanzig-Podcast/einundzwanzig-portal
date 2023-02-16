8625bfc5fdbd08ea66fe87a187c80c4eaed5f59b
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-16 21:01:53 +0100

    deactivate bookcases

--- app/Console/Commands/OpenBooks/SyncOpenBooks.php
+++ app/Console/Commands/OpenBooks/SyncOpenBooks.php
@@ -28,32 +28,43 @@ class SyncOpenBooks extends Command
     {
         $response = Http::post('https://openbookcase.de/api/listarea/83.08995477111446/-200.56640625000003/-38.13455657705413/221.30859375000003');
 
+        $ids = collect($response->json()['cases'])->pluck('id');
+
         foreach ($response->json()['cases'] as $case) {
-            BookCase::updateOrCreate(
-                [
-                    'id' => $case['id'],
-                ],
-                [
-                    'title'       => $case['title'],
-                    'latitude'         => (float)$case['lat'],
-                    'longitude'         => (float)$case['lon'],
-                    'address'     => $case['address'],
-                    'type'        => $case['type'],
-                    'open'        => $case['open'],
-                    'comment'     => $case['comment'],
-                    'contact'     => $case['contact'],
-                    'bcz'         => $case['bcz'],
-                    'digital'     => $case['digital'] ?? false,
-                    'icontype'    => $case['icontype'],
-                    'deactivated' => $case['deactivated'],
-                    'deactreason' => $case['deactreason'],
-                    'entrytype'   => $case['entrytype'],
-                    'homepage'    => $case['homepage'],
-                    'created_by'  => 1,
-                ]
-            );
+            try {
+                BookCase::updateOrCreate(
+                    [
+                        'id' => $case['id'],
+                    ],
+                    [
+                        'id'          => $case['id'],
+                        'title'       => $case['title'],
+                        'latitude'    => (float) $case['lat'],
+                        'longitude'   => (float) $case['lon'],
+                        'address'     => $case['address'],
+                        'type'        => $case['type'],
+                        'open'        => $case['open'],
+                        'comment'     => $case['comment'],
+                        'contact'     => $case['contact'],
+                        'bcz'         => $case['bcz'],
+                        'digital'     => $case['digital'] ?? false,
+                        'icontype'    => $case['icontype'],
+                        'deactivated' => $case['deactivated'],
+                        'deactreason' => $case['deactreason'],
+                        'entrytype'   => $case['entrytype'],
+                        'homepage'    => $case['homepage'],
+                        'created_by'  => 1,
+                    ]
+                );
+            } catch (\Exception $e) {
+                $this->error($e->getMessage());
+            }
+
+            return Command::SUCCESS;
         }
 
-        return Command::SUCCESS;
+        BookCase::query()
+                ->whereNotIn('id', $ids)
+                ->update(['deactivated' => true]);
     }
 }
