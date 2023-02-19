df2d3d03bad9c1f52248056361ec6adf98786715
Shift <shift@laravelshift.com>
2023-02-19 16:19:05 +0000

    Remove unnecessary `$model` property

--- database/factories/CityFactory.php
+++ database/factories/CityFactory.php
@@ -9,13 +9,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CityFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = City::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
