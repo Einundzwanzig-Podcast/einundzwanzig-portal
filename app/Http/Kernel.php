4cce44dfeab5d9916c398cd09a5960ab9454e168
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-19 23:12:56 +0100

    needMeetup added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -84,5 +84,6 @@ class Kernel extends HttpKernel
         'signed'           => \App\Http\Middleware\ValidateSignature::class,
         'throttle'         => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified'         => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'need-meetup'      => NeedMeetupMiddleware::class,
     ];
 }
