cd71bad38d7ac58f0f7e695abdc466314f35b3d2
Shift <shift@laravelshift.com>
2023-02-19 16:19:22 +0000

    Shift core files

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,19 +65,19 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             SetTimezoneMiddleware::class,
         ],
     ];
 
     /**
-     * The application's route middleware.
-     * These middleware may be assigned to groups or used individually.
+     * The application's middleware aliases.
+     * Aliases may be used to conveniently assign middleware to routes and groups.
      *
      * @var array<string, class-string|string>
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
