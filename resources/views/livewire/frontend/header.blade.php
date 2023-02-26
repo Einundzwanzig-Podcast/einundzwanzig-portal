2e5a7239105c99fdab959d58ed08905bc8396706
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-26 14:37:38 +0100

    header full width

--- resources/views/livewire/frontend/header.blade.php
+++ resources/views/livewire/frontend/header.blade.php
@@ -1,5 +1,5 @@
 <header x-data="{ open: false }" @keydown.window.escape="open = false" class="relative isolate z-10 bg-white mb-4">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <nav class="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
             <a href="/" class="-m-1.5 p-1.5">
                 <span class="sr-only">Einundzwanzig Portal</span>
