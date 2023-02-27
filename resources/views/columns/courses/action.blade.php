384740daebd9b0b51794ba72e6c095bd2acd36f2
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-27 18:39:54 +0100

    whitespace-nowrap

--- resources/views/columns/courses/action.blade.php
+++ resources/views/columns/courses/action.blade.php
@@ -1,23 +1,23 @@
 <div class="flex flex-col space-y-1">
     @if($row->course_events_count > 0)
         <div>
-            <x-button xs amber wire:click="courseSearch({{ $row->id }})">
+            <x-button class="whitespace-nowrap" xs amber wire:click="courseSearch({{ $row->id }})">
                 {{ __('Show dates') }} [{{ $row->course_events_count }}]
             </x-button>
         </div>
     @else
         <div>
-            <x-button xs outline wire:click="courseSearch({{ $row->id }})">
+            <x-button class="whitespace-nowrap" xs outline wire:click="courseSearch({{ $row->id }})">
                 {{ __('Show dates') }}
             </x-button>
         </div>
     @endif
     @can('update', $row)
         <div>
-            <x-button amber xs :href="route('course.form.course', ['course' => $row])">
+            <x-button class="whitespace-nowrap" amber xs :href="route('course.form.course', ['course' => $row])">
                 <i class="fa fa-thin fa-edit"></i>
                 {{ __('Edit') }}
-            </x-buttonamber>
+            </x-button>
         </div>
     @endcan
 </div>
