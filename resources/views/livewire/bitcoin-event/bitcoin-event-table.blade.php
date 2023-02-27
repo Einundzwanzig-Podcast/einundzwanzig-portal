11591806a72b3bbef06152fa3c2fb58b1432419d
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-02-27 19:27:08 +0100

    Bitcoin Events added

--- resources/views/livewire/bitcoin-event/bitcoin-event-table.blade.php
+++ resources/views/livewire/bitcoin-event/bitcoin-event-table.blade.php
@@ -140,6 +140,16 @@
                 </div>
             </div>
 
+            <div class="md:flex md:items-center md:justify-between">
+                <div class="min-w-0 flex-1">
+                    <h2 class="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
+                        {{ __('Bitcoin Events') }}
+                    </h2>
+                </div>
+                <div class="mt-4 flex md:mt-0 md:ml-4">
+                    {{----}}
+                </div>
+            </div>
             <livewire:tables.bitcoin-event-table :country="$country->code"/>
         </div>
     </section>
