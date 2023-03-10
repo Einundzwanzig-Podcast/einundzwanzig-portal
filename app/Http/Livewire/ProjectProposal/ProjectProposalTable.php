58dab95dceb261196d293a78fe35660c7d51be7c
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-03-10 23:52:15 +0100

    voting added

--- app/Http/Livewire/ProjectProposal/ProjectProposalTable.php
+++ app/Http/Livewire/ProjectProposal/ProjectProposalTable.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\ProjectProposal;
 
 use App\Models\Country;
 use Livewire\Component;
+use RalphJSmit\Laravel\SEO\Support\SEOData;
 
 class ProjectProposalTable extends Component
 {
@@ -11,6 +12,12 @@ class ProjectProposalTable extends Component
 
     public function render()
     {
-        return view('livewire.project-proposal.project-proposal-table');
+        return view('livewire.project-proposal.project-proposal-table')->layout('layouts.app', [
+            'SEOData' => new SEOData(
+                title: __('Project Proposal'),
+                description: __('Submit a project proposal and let the community vote on it through the elected voters. All other community members can also vote.'),
+                image: asset('img/voting.jpg')
+            ),
+        ]);
     }
 }
