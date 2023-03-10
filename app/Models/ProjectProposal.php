255bf2a2d8870e06daa0dc7eebd50726c30f3de9
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-03-11 00:10:30 +0100

    voting added

--- app/Models/ProjectProposal.php
+++ app/Models/ProjectProposal.php
@@ -6,40 +6,50 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Cookie;
 use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class ProjectProposal extends Model implements HasMedia
 {
     use InteractsWithMedia;
     use HasFactory;
+    use HasSlug;
 
     /**
      * The attributes that aren't mass assignable.
-     *
      * @var array
      */
     protected $guarded = [];
 
     /**
      * The attributes that should be cast to native types.
-     *
      * @var array
      */
     protected $casts = [
-        'id' => 'integer',
+        'id'      => 'integer',
         'user_id' => 'integer',
     ];
 
     protected static function booted()
     {
         static::creating(function ($model) {
-            if (! $model->created_by) {
+            if (!$model->created_by) {
                 $model->created_by = auth()->id();
             }
         });
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+                          ->generateSlugsFrom(['name'])
+                          ->saveSlugsTo('slug')
+                          ->usingLanguage(Cookie::get('lang', config('app.locale')));
     }
 
     public function registerMediaConversions(Media $media = null): void
