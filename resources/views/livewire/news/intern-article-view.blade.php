2eb7315ac01aa32f83a6e283866afa5b0db6f7b1
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-03-15 13:59:07 +0100

    max-w-full

--- resources/views/livewire/news/intern-article-view.blade.php
+++ resources/views/livewire/news/intern-article-view.blade.php
@@ -52,7 +52,7 @@
                     </div>
                 </div>
                 <div class="-mt-12 -ml-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-                    <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="{{ $libraryItem->getFirstMediaUrl('main') }}" alt="{{ $libraryItem->name }}">
+                    <img class="w-[48rem] max-w-full rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="{{ $libraryItem->getFirstMediaUrl('main') }}" alt="{{ $libraryItem->name }}">
                     <figcaption class="mt-3 flex text-sm text-gray-200">
                         <!-- Heroicon name: mini/camera -->
                         <svg class="h-5 w-5 flex-none text-gray-400" xmlns="http://www.w3.org/2000/svg"
