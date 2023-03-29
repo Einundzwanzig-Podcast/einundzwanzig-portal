df18a5096837309b0958f454e0ad90e30f57d293
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-03-29 17:01:10 +0200

    undoPoint fix

--- resources/views/livewire/news/intern-article-view.blade.php
+++ resources/views/livewire/news/intern-article-view.blade.php
@@ -287,7 +287,7 @@
                                 <div
                                     class="prose md:prose-lg prose-invert mx-auto mt-5 text-gray-100 lg:col-start-1 lg:row-start-1 lg:max-w-none">
 
-                                    <<x-markdown class="leading-normal">
+                                    <x-markdown class="leading-normal">
                                         {!! $libraryItem->value_to_be_paid !!}
                                     </x-markdown>
 
