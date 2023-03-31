12e3f3269b874b39040b449cb03be5aaddaec9cd
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-03-31 15:58:06 +0200

    mempool weather added

--- resources/views/livewire/banner/mempool-weather.blade.php
+++ resources/views/livewire/banner/mempool-weather.blade.php
@@ -10,7 +10,7 @@
         <div class="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#FABE75] to-[#F7931A] opacity-30"
              style="clip-path: polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)"></div>
     </div>
-    <div class="w-full flex flex-col sm:flex-row justify-center">
+    <div class="w-full flex flex-col sm:flex-row sm:space-x-4 justify-center">
         <div class="text-md leading-6 text-gray-900 text-center max-w-screen-2xl">
             {{ $weather }}
         </div>
