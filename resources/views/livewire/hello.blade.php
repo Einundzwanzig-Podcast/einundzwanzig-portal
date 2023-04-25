aa4979942bee331293431bb940e46f18c6d1eb0f
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-04-25 23:38:16 +0200

    TTS

--- resources/views/livewire/hello.blade.php
+++ resources/views/livewire/hello.blade.php
@@ -2,13 +2,15 @@
     <div
         class="mt-10 flex flex-col items-center justify-center gap-x-6 bg-white pb-12">
         @if(!$invoicePaid)
-            <div class="text-xl font-semibold text-gray-900 py-6">
+            <div class="text-xl font-semibold text-gray-900 py-6 px-2">
                 Deine Nachricht wird live vorgelesen.
+                Fülle zuerst das Textfeld mit deiner Nachricht aus
+                und bezahle erst danach mit Lightning.
             </div>
             <div class="text-xl font-semibold text-gray-900 py-6 w-full px-12">
                 <x-textarea wire:model="message" label="Deine Nachricht hier" corner-hint="max. 255 Zeichen"/>
             </div>
-            <div class="text-xl font-semibold text-gray-900 py-6">
+            <div class="text-xl font-semibold text-gray-900 py-6 px-2">
                 {{ __('Click QR-Code to open your wallet') }}
             </div>
             <div class="flex justify-center" wire:key="qrcode">
