7db89721b667597c2948bf39f1e63124c6fa3441
fsociety <fsociety.mp3@pm.me>
2024-04-14 13:43:11 +0200

    feat: update iframe source in follow-the-rabbit view

--- resources/views/livewire/helper/follow-the-rabbit.blade.php
+++ resources/views/livewire/helper/follow-the-rabbit.blade.php
@@ -3,7 +3,7 @@
     <livewire:frontend.header :country="\App\Models\Country::query()->where('code', 'de')->first()"/>
 
     <div class="px-2 sm:px-24">
-        <iframe allowfullscreen="true" src="https://www.easyzoom.com/embed/afa0fda48faa425eb20f323e03cae346" width="100%"
+        <iframe allowfullscreen="true" src="https://www.easyzoom.com/imageaccess/1322a81aa55e4fd6a3188f6217476652" width="100%"
                 style="height: 90vh;"></iframe>
     </div>
 
