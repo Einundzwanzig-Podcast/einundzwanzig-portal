89d2f3024146a553a961b8a5d72609ff9256ae8f
fsociety <fsociety.mp3@pm.me>
2024-09-10 21:58:19 +0200

    feat: update payment details in WebLN test view
    This commit modifies the WebLN test view in the Einundzwanzig portal. Changes include the recipient name change from "Bob" to "The Ben", addition of payment hash and payment request information, and updates the button text to "Pay Invoice".

--- resources/views/livewire/test/web-l-n.blade.php
+++ resources/views/livewire/test/web-l-n.blade.php
@@ -1,11 +1,14 @@
 <div x-data="webln(@this)" class="p-2 sm:p-4" wire:ignore>
     <div class="font-mono space-y-1 p-2 sm:p-4 text-white break-words">
-        <div class="text-xs sm:text-base break-words">Test Payment from WebLN to "Bob"</div>
+        <div class="text-xs sm:text-base break-words">Test Payment from WebLN to "The Ben"</div>
         <div class="text-xs sm:text-base break-words">1 sat</div>
-        <div class="text-xs sm:text-base break-words">keysend to: 0363662a4ae8b8b7a73d9f4e459a9b25d4786f4ecc7315b5401934f3a2ef609750</div>
+        <div class="text-xs sm:text-base break-words">hash: {{ $invoice['payment_hash'] }}</div>
+        <div class="text-xs sm:text-base break-words">payment_request: {{ $invoice['payment_request'] }}</div>
         <div class="mt-6">
             <div class="flex justify-center">
-                <button x-on:click="keySendMethod" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Pay</button>
+                <button x-on:click="pay" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Pay Invoice
+                </button>
             </div>
         </div>
     </div>
